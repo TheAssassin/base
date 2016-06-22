cbf97cd94f8d8643b563a7998d936f11f9ad308f
TheAssassin <theassassin@users.noreply.github.com>
2016-06-22 19:37:23 +0000

    Attempt to be able to import server_plugin Python package from several server binary locations

--- src/python/_server_plugin_init.py
+++ src/python/_server_plugin_init.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+# allow the plugin to import server_plugin
+def fix_pythonpath():
+    for path in [".", "..", "../python", "../src/python"]:
+        full_path = os.path.abspath(os.path.join(os.curdir, path))
+        sys.path.insert(0, full_path)
+
+fix_pythonpath()
+
 from python_server_plugin import ffi
 from server_plugin.re_api import log_server_console
 from server_plugin.util import Privileges
